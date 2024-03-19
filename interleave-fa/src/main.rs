afa28a84e2108ae2c40f045b0d0d0e1a4592a280
Timofey Prodanov <timofey.prodanov@gmail.com>
2024-03-19 10:36:52 +0100

    interleave-fa without large intermediate files

--- interleave-fa/src/main.rs
+++ interleave-fa/src/main.rs
@@ -18,54 +18,46 @@ fn split_and_interleave() -> io::Result<()> {
     assert_eq!(args.len(), 3);
 
     let in_filename = &args[1];
-    let f_in = BufReader::new(File::open(in_filename)?);
-    let mut f_in = BufReader::new(MultiGzDecoder::new(f_in));
+    let mut f_in = BufReader::new(MultiGzDecoder::new(BufReader::new(File::open(in_filename)?)));
     let out_filename = &args[2];
+    let tmp_filename = format!("{}.tmp", out_filename);
+    println!("{}  {}  {}", in_filename, out_filename, tmp_filename);
     let mut f_out = BufWriter::with_capacity(131_072,
-        GzEncoder::new(File::create(out_filename)?, Compression::default()));
-    let tmp_filename = format!("{}.tmp.fasta", out_filename);
-    let mut f_tmp = BufWriter::with_capacity(131_072, File::create(&tmp_filename)?);
+        GzEncoder::new(BufWriter::new(File::create(&tmp_filename)?), Compression::default()));
 
     let mut buf = Vec::with_capacity(1024);
     read_line(&mut f_in, &mut buf)?;
     read_line(&mut f_in, &mut buf)?;
-    f_tmp.write_all(&buf)?;
-    let mut count1 = 1;
+    let mut count = 1;
     loop {
         buf.clear();
         let n = read_line(&mut f_in, &mut buf)?;
         if buf[n - 3] == b'.' && buf[n - 2] == b'1' {
             break;
         }
-
         read_line(&mut f_in, &mut buf)?;
-        f_tmp.write_all(&buf)?;
-        count1 += 1;
+        count += 1;
     }
-    f_tmp.flush()?;
-    std::mem::drop(f_tmp);
 
     let old_buf = buf;
     let mut buf = Vec::with_capacity(1024);
-    let mut f_tmp = BufReader::new(File::open(&tmp_filename)?);
-    read_line(&mut f_tmp, &mut buf)?;
-    read_line(&mut f_tmp, &mut buf)?;
+    let mut f_in1 = BufReader::new(MultiGzDecoder::new(BufReader::new(File::open(in_filename)?)));
+    read_line(&mut f_in1, &mut buf)?;
+    read_line(&mut f_in1, &mut buf)?;
     buf.extend_from_slice(&old_buf);
     read_line(&mut f_in, &mut buf)?;
     f_out.write_all(&buf)?;
 
-    for _ in 1..count1 {
+    for _ in 1..count {
         buf.clear();
-        read_line(&mut f_tmp, &mut buf)?;
-        read_line(&mut f_tmp, &mut buf)?;
+        read_line(&mut f_in1, &mut buf)?;
+        read_line(&mut f_in1, &mut buf)?;
         assert!(read_line(&mut f_in, &mut buf)? > 0);
         read_line(&mut f_in, &mut buf)?;
         f_out.write_all(&buf)?;
     }
-    std::mem::drop(f_tmp);
+    f_out.flush()?;
     std::mem::drop(f_out);
-
-    std::fs::remove_file(&tmp_filename)?;
 
     let last_recs = String::from_utf8_lossy(&buf);
     let last_recs: Vec<_> = last_recs.split("\n").collect();
@@ -73,6 +65,8 @@ fn split_and_interleave() -> io::Result<()> {
     buf.clear();
     buf.resize(16, 0);
     assert_eq!(f_in.read(&mut buf)?, 0);
+
+    std::fs::rename(&tmp_filename, &out_filename)?;
     Ok(())
 }
 
--- prepare_seqs.py
+++ prepare_seqs.py
@@ -1,93 +1,170 @@
 #!/usr/bin/env python3
 
+import os
+import sys
+import operator
+from collections import OrderedDict, defaultdict
 import argparse
 import pysam
-import sys
-import os
 from Bio import SeqIO
 from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 
 import common
 
 
-def add_fasta_source(source, filename, out_seqs):
-    with common.open(filename) as fasta:
-        for record in SeqIO.parse(fasta, 'fasta'):
-            out_seqs.append(SeqRecord(
-                record.seq,
-                id=f'{source}::{record.id}',
-                description=f'{len(record.seq)} bp'
-            ))
+def get_params(record, alns, qseqs):
+    p = argparse.Namespace()
+    p.qname = record.query_name
+    p.qstart = record.query_alignment_start
+    p.qend = record.query_alignment_end
+
+    if '.' in p.qname:
+        p.sample, hap = p.qname.split('.')
+        p.hap = int(hap) - 1
+    else:
+        p.sample = p.qname
+        p.hap = 0
+
+    p.rname = record.reference_name
+    p.rstart = record.reference_start
+    p.rend = record.reference_end
+    p.rlen = alns.get_reference_length(p.rname)
+    p.edit = record.get_tag('NM')
+    p.nmatches = p.rend - p.rstart - p.edit
+    p.rrem = p.rlen - p.rend
+    p.clip = (p.rstart + p.rrem) / p.rlen
+    p.divergence = (p.edit + p.rstart + p.rrem) / p.rlen
+
+    if p.qname not in qseqs:
+        assert record.seq is not None and 'H' not in record.cigarstring
+        qseqs[p.qname] = record.seq
+    p.qseq = qseqs[p.qname]
+    p.qlen = len(p.qseq)
+    return p
 
 
-def add_vcf_source(source, filename, ref_seq, chrom, start, end, out_seqs):
-    # Output indels and SNPs over 5 bp.
-    SNP_LEN = 5
+def process_alns(alns, seqs, out, clipping, max_divergence, add_padding):
+    coords = defaultdict(list)
+    gts = defaultdict(lambda: (set(), set()))
+    locus = next(iter(seqs.values())).split('*')[0]
+    n_ext = 0
+    qseqs = {}
+    for record in alns:
+        p = get_params(record, alns, qseqs)
+        if record.is_unmapped:
+            gts[p.sample][p.hap].add('<MISSING>')
+            sys.stderr.write(f'!!! {p.qname} is unmapped\n')
+            out.write(f'{p.sample}\t{p.hap+1}\t*\t*\t*\tNA\tNA\tNA\tNA\t!!\t*\n')
+            continue
 
-    with pysam.VariantFile(filename) as vcf:
-        prev_end = 0
-        for var in vcf.fetch(chrom, start, end):
-            assert var.start >= prev_end, f'Source {source} ({filename}) contains overlapping variants'
-            alleles = var.alleles
-            ref_allele = alleles[0]
-            if max(map(len, alleles)) < SNP_LEN and all(len(allele) == len(ref_allele) for allele in alleles[1:]):
-                continue
+        if p.qname not in coords and p.qname == p.sample:
+            gts[p.sample][1].add('<HAPLOID>')
+        is_redundant = False
+        for prev_start, prev_end, prev_matches in coords[p.qname]:
+            overl_frac = max(0, min(prev_end, p.qend) - max(prev_start, p.qstart)) / p.rlen
+            # Alignment is not good enough or overlaps an old one.
+            if p.nmatches + 10 <= prev_matches or overl_frac >= 0.5:
+                is_redundant = True
+        if is_redundant:
+            continue
 
-            var_end = var.start + len(ref_allele)
-            assert start < var_end and var.start < end
-            prefix = ref_seq[ : max(0, var.start - start)]
-            suffix = ref_seq[min(len(ref_seq), var_end - start) : ]
-            for i, alt in enumerate(alleles[1:], 1):
-                seq = prefix + alt + suffix
-                out_seqs.append(SeqRecord(
-                    Seq(seq),
-                    id=f'{source}::{var.start + 1}:{i}',
-                    description=f'{len(seq)} bp, variant {chrom}:{var.start + 1}-{var_end} ({len(ref_allele)} bp), '
-                        f'{len(alleles)} alleles'))
+        coords[p.qname].append((p.qstart, p.qend, p.nmatches))
+        allele = None
+        status = ''
+        if p.clip >= clipping:
+            status += '!'
+            allele = '<MISSING>'
+        elif p.clip >= 0.005:
+            status += '~'
+        else:
+            status += '+'
+
+        if p.divergence >= max_divergence:
+            status += '!'
+            allele = '<MISSING>'
+        elif p.divergence >= 0.01:
+            status += '~'
+        else:
+            status += '+'
+
+        if allele is None:
+            if record.seq is None:
+                assert 'H' not in record.cigarstring
+                assert not record.is_reverse
+                seq = p.qseq
+            else:
+                seq = record.seq
+
+            if add_padding:
+                assert not record.is_reverse
+                seq = seq[max(0, p.qstart - p.rstart):min(p.qlen, p.qend + p.rrem)]
+            else:
+                assert p.qend <= len(seq)
+                seq = seq[p.qstart:p.qend]
+            assert seq
+
+            allele = seqs.get(seq)
+            if allele is None:
+                n_ext += 1
+                allele = f'{locus}*ext{n_ext:03}'
+                assert allele not in seqs
+                seqs[seq] = allele
+        gts[p.sample][p.hap].add(allele)
+        out.write(f'{p.sample}\t{p.hap+1}\t{p.qstart+1}-{p.qend}\t{"-" if record.is_reverse else "+"}\t'
+            f'{p.rname}\t{p.rstart},{p.rrem}\t{p.clip:.5g}\t'
+            f'{p.edit}\t{p.divergence:.5g}\t{status}\t{allele}\n')
+    return gts
+
+
+def alleles_to_str(alleles):
+    if not alleles:
+        return '<UNKNOWN>'
+    count_special = sum(allele.startswith('<') for allele in alleles)
+    if count_special == len(alleles) or count_special == 0:
+        return ';'.join(alleles)
+    return ';'.join(allele for allele in alleles if not allele.startswith('<'))
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Extract relevant sequences from various sources.')
-    parser.add_argument('-r', '--reference', metavar=('NAME', 'FILE'), nargs=2, required=True,
-        help='Reference name and FASTA file.')
-    parser.add_argument('-l', '--locus', metavar='FILE', required=True,
-        help='Locus coordinates in BED format.')
-    parser.add_argument('-s', '--source', metavar='FILE', action='append',
-        help='Source in FASTA or VCF.GZ format. '
-            'Fasta sequence should be already reverse-complemented, if needed.')
-    parser.add_argument('-o', '--output', metavar='FILE', required=True,
-        help='Output FASTA file.')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--samples', required=True,
+        help='Full set of samples.')
+    parser.add_argument('-r', '--ref', required=True,
+        help='Reference sequences in FASTA format.')
+    parser.add_argument('-a', '--aln', required=True,
+        help='Input alignment file between assembly and IPD sequences.')
+    parser.add_argument('-o', '--output', required=True,
+        help='Output directory.')
+    parser.add_argument('-c', '--clipping', type=float, default=0.01,
+        help='Maximum allowed clipping as fraction of allele length [%(default)s].')
+    parser.add_argument('-d', '--divergence', type=float, default=0.05,
+        help='Maximum allowed divergence [%(default)s].')
+    parser.add_argument('--add-padding', action='store_true',
+        help='Add padding to new sequences, according to unmapped first/last several bp.')
     args = parser.parse_args()
 
-    with common.open(args.locus) as f:
-        line = next(f).strip().split()
-        chrom = line[0]
-        start = int(line[1])
-        end = int(line[2])
+    with common.open(args.ref) as inp:
+        seqs = OrderedDict((str(record.seq).upper(), record.id) for record in SeqIO.parse(inp, 'fasta'))
+    pysam_verbosity = pysam.set_verbosity(0)
+    with pysam.AlignmentFile(args.aln, require_index=False) as alns, \
+            common.open(os.path.join(args.output, 'alns.csv'), 'w') as out_alns:
+        pysam.set_verbosity(pysam_verbosity)
+        out_alns.write('sample\thaplotype\tcoords\tstrand\tallele'
+            '\tclip\tfrac_clip\tedit\tdivergence\tstatus\tnew_allele\n')
+        gts = process_alns(alns, seqs, out_alns, args.clipping, args.divergence, args.add_padding)
 
-    ref = pysam.FastaFile(args.reference[1])
-    ref_seq = ref.fetch(chrom, start, end).upper()
-    out_seqs = [SeqRecord(
-        Seq(ref_seq),
-        id=args.reference[0],
-        description=f'{chrom}:{start+1}-{end}, {len(ref_seq)} bp')]
-
-    sources = set()
-    for filename in args.source:
-        source = os.path.basename(filename).split('.', 1)[0]
-        if source in sources:
-            assert False, f'Source {source} appears twice'
-        sources.add(source)
-
-        if filename.endswith('.fa') or filename.endswith('.fasta') \
-                or filename.endswith('.fa.gz') or filename.endswith('.fasta.gz'):
-            add_fasta_source(source, filename, out_seqs)
-        else:
-            add_vcf_source(source, filename, ref_seq, chrom, start, end, out_seqs)
-
-    with common.open(args.output, 'w') as out:
-        SeqIO.write(out_seqs, out, 'fasta')
+    with common.open(args.samples) as samples_f, \
+            common.open(os.path.join(args.output, 'extended.fa.gz'), 'w') as out_fa, \
+            common.open(os.path.join(args.output, 'true_gts.csv'), 'w') as out_gt:
+        out_gt.write('sample\talleles1\talleles2\n')
+        samples = list(map(str.strip, samples_f))
+        for sample in sorted(set(samples) | set(gts)):
+            alleles1 = alleles_to_str(gts[sample][0])
+            alleles2 = alleles_to_str(gts[sample][1])
+            out_gt.write(f'{sample}\t{alleles1}\t{alleles2}\n')
+        for seq, name in seqs.items():
+            out_fa.write(f'>{name} {len(seq)} bp\n{seq}\n')
 
 
 if __name__ == '__main__':
