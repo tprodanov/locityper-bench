7d8e4461666d5f36781a54ca9c58ceaf321ff13b
Timofey Prodanov <timofey.prodanov@gmail.com>
2024-11-23 11:24:32 +0100

    Do not add original haplotypes when mutating

--- mutate_haplotypes.py
+++ mutate_haplotypes.py
@@ -44,18 +44,17 @@ def write_fa(name, seq, out):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', metavar='FILE', required=True,
-        help='Input FASTA file')
+        help='Input FASTA file.')
     parser.add_argument('-o', '--output', metavar='FILE', required=True,
-        help='Output FASTA file')
+        help='Output FASTA file.')
     parser.add_argument('-m', '--mut-rate', metavar='FLOAT', default=0.0001, type=float,
-        help='Chance for every given nucleotide to mutate in a given haplotype')
-    parser.add_argument('-c', '--copies', metavar='INT', default=2, type=int,
-        help='How many copies (in total) should every haplotype appear in?'
-             ' 1: do nothing, 2: mutate each haplotype once.')
+        help='Chance for every given nucleotide to mutate in a given haplotype.')
+    parser.add_argument('-c', '--copies', metavar='INT', default=1, type=int,
+        help='Total number of mutated copies (in total) per haplotype.')
     parser.add_argument('-s', '--seed', metavar='INT', type=int,
-        help='Optional: random seed')
+        help='Optional: random seed.')
     parser.add_argument('-a', '--always', action='store_true',
-        help='If no mutations were added, add one randomly')
+        help='If no mutations were added, add one randomly.')
     args = parser.parse_args()
 
     if args.seed is not None:
@@ -65,8 +64,7 @@ def main():
         for record in SeqIO.parse(inp, 'fasta'):
             name = record.id
             seq = str(record.seq)
-            write_fa(name, seq, out)
-            for i in range(1, args.copies):
+            for i in range(1, args.copies + 1):
                 new_seq, count = mutate(seq, args.mut_rate, args.always)
                 write_fa(f'{name}-mut{i} {count} mutations', new_seq, out)
 
